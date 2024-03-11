65aeae5d9b77756dee8f9aad1f816a7dc155963e
MjChen120 <156042164+MjChen120@users.noreply.github.com>
2024-03-11 19:44:34 -0400

    Update data_tabling

--- scripts/03-data_tabling.R
+++ scripts/03-data_tabling.R
@@ -1,113 +1,188 @@
 #### Preamble ####
-# Purpose: Study 2 Table Creating
-# Author: Mingjia Chen, Hadi Ahmad, Tianen (Evan) Hao
-# Date: 17 February 2024
+# Purpose: GSS Tables and Graphs for the Paper
+# Author: Mingjia Chen, Catherine Punnoose, Tianen (Evan) Hao
+# Date: 11 March 2024 
 # Contact: mingjia.chen@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: R 4.3.2
+# Pre-requisites: R 4.3.2, Cropped_Data.csv, cleaned_categorized_data.csv
 
 #download the packages if necessary, then load the packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(haven, psych,dplyr,tidyverse, knitr,kableExtra)
+pacman::p_load(janitor,here,dplyr,tidyverse,knitr,kableExtra,ggplot2)
 
 # Load the psych package
-library(psych)
-library(dplyr)
+library(janitor)
 library(tidyverse)
 library(knitr)
+library(dplyr)
+library(kableExtra)
+library(here)
+library(ggplot2)
 
-data <- read_csv("FW_satisfaction-Study_2-data.csv")
+# Overview of the Categorized Data 
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+# Delete the column
+data <- data[c(2:ncol(data))]
+colnames(data)[1] <- "Work Hours/Years"
+# Get rid of the first letter X for each of the years
+colnames(data) <- gsub("X", "", colnames(data))
 
-##### Table 1. Study 2 Means, Standard Deviations, and Correlations################################################################################
-# Select the variables for correlation analysis
-selected_variables <-data[, c("jobsat1", "jobsat2", "FWDfwagency", "jobaut", "jobaut2", "locus", "ess_kind", "selfest", "selfeff", "selfcontrol")]
+data_selected <- data[c("Work Hours/Years", "1998","2008","2021","Total")]
+kable(data_selected,row.names = FALSE)|>
+  kable_styling() |>
+  row_spec(6, hline_after = TRUE)
 
-# Compute pairwise correlations
-correlation_matrix <- cor(selected_variables, use = "pairwise.complete.obs")
+# Overview of the 1998 Survey Data 
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+data <- data[1:6,]
 
-# modifying the format so it matches to the correlation table like in paper
-## making the upper right part zero
+ggplot(data, aes(x = "", y = X1998, fill = work_hours)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  scale_fill_brewer(palette = "Set3") +  # Use a color palette
+  theme_void() +  # Remove background and gridlines
+  labs(title = "1998 Survey Results") +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
 
-## Add Mean and SD
-M <- c(mean(selected_variables$jobsat1), mean(selected_variables$jobsat2,na.rm = TRUE), mean(selected_variables$FWDfwagency),
-       mean(selected_variables$jobaut,na.rm = TRUE),mean(selected_variables$jobaut2,na.rm = TRUE),mean(selected_variables$locus),
-       mean(selected_variables$ess_kind),mean(selected_variables$selfest),mean(selected_variables$selfeff),
-       mean(selected_variables$selfcontrol))
+# Overview of the 2008 Survey Data 
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+data <- data[1:6,]
 
-SD <- c(sd(selected_variables$jobsat1), sd(selected_variables$jobsat2,na.rm = TRUE), sd(selected_variables$FWDfwagency),
-       sd(selected_variables$jobaut,na.rm = TRUE),sd(selected_variables$jobaut2,na.rm = TRUE),sd(selected_variables$locus),
-       sd(selected_variables$ess_kind),sd(selected_variables$selfest),sd(selected_variables$selfeff),
-       sd(selected_variables$selfcontrol))
+ggplot(data, aes(x = "", y = X2008, fill = work_hours)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  scale_fill_brewer(palette = "Set3") +  # Use a color palette
+  theme_void() +  # Remove background and gridlines
+  labs(title = "2008 Survey Results") +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
 
-#choose desired rows
-correlation_matrix <- correlation_matrix[,c('jobsat1','jobsat2','FWDfwagency','jobaut','jobaut2')]
+# Overview of the 2021 Survey Data 
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+data <- data[1:6,]
 
-correlation_matrix<- cbind(M, SD, correlation_matrix)
+ggplot(data, aes(x = "", y = X2021, fill = work_hours)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  scale_fill_brewer(palette = "Set3") +  # Use a color palette
+  theme_void() +  # Remove background and gridlines
+  labs(title = "2021 Survey Results") +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
 
-#rename columns and rows in r
-colnames(correlation_matrix)[colnames(correlation_matrix) == "jobsat1"] <- "Job satisfaction (T1)"
-colnames(correlation_matrix)[colnames(correlation_matrix) == "jobsat2"] <- "Job satisfaction (T2)"
-colnames(correlation_matrix)[colnames(correlation_matrix) == "FWDfwagency"] <- "Belief in free will (T1)"
-colnames(correlation_matrix)[colnames(correlation_matrix) == "jobaut"] <- "Job autonomy (T1)"
-colnames(correlation_matrix)[colnames(correlation_matrix) == "jobaut2"] <- "Job autonomy (T2)"
+# Average Working Hours Comparison between Financial Events
+data <- read.csv(here:here("/input/data/Cleaned_Data.csv"))
+# Get rid of the first letter X for each of the years
+colnames(data)[1] <- "work_hours"
+colnames(data) <- gsub("X", "", colnames(data))
 
-rownames(correlation_matrix)[rownames(correlation_matrix) == "jobsat1"] <- "Job satisfaction (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "jobsat2"] <- "Job satisfaction (T2)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "FWDfwagency"] <- "Belief in free will (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "jobaut"] <- "Job autonomy (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "jobaut2"] <- "Job autonomy (T2)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "locus"] <- "Trait locus of control (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "ess_kind"] <- "Implicit beliefs (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "selfest"] <- "Trait self-esteem (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "selfeff"] <- "Job self-efficacy (T1)"
-rownames(correlation_matrix)[rownames(correlation_matrix) == "selfcontrol"] <- "Trait self-control (T1)"
+#Histogram of average working hours for 2008, 2021, 2022, and Total
+hist_data <- select(data, work_hours,"2010","2018","2021")
+hist_data <- hist_data[5:94,]
+
+num <- 3 #number of years
+sum <- rep(0,num)
+total_people <- rep(0,num)
+for(i in 1:num){
+  total_people[i] <- sum(hist_data[, i+1])
+}
+hist_data[, 1] <- sapply(hist_data[, 1], as.numeric)
+hist_data[90,1] <- 90
+
+sum[1] <- sum(hist_data$"2010" * hist_data$work_hours)
+sum[2] <- sum(hist_data$"2018" * hist_data$work_hours)
+sum[3] <- sum(hist_data$"2021" * hist_data$work_hours)
 
 
-#### Table 2 (Appendix) - Correlations between free will subscales and job satisfaction ########################################
-#Correlations between free will subscales and job satisfaction
-# Select the variables for correlation analysis
-selected_variables_2 <-data[, c("jobsat1", "jobsat2", "FWDfw", "FWDagency", "FWDmoral","FWDresp",
-                              "FWDpower","FWDlimit","FWDall","FWDfwagency","FWself","FWgeneral")]
+averages <- round(sum/total_people) 
+years <- c("2010","2018","2021")
 
-# Compute pairwise correlations
-correlation_matrix_2 <- cor(selected_variables_2, use = "pairwise.complete.obs")
+average_hours <- data.frame(cbind(averages,years))
 
-#choose desired rows and columns
-correlation_matrix_2 <- correlation_matrix_2[c('FWDfwagency','FWDall','FWDfw','FWDagency','FWDmoral','FWDpower',
-                                               "FWDresp",'FWDlimit','FWself','FWgeneral'),]
-correlation_matrix_2 <- correlation_matrix_2[,c('jobsat1','jobsat2')]
+ggplot(average_hours,aes(x=years,y=averages)) +
+  geom_bar(stat="identity") + 
+  theme_minimal() + # Make the theme neater
+  labs(x = "Year", y = "Average Working Hour") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
 
-#rename columns and rows 
-colnames(correlation_matrix_2)[colnames(correlation_matrix_2) == "jobsat1"] <- "Job satisfaction T1"
-colnames(correlation_matrix_2)[colnames(correlation_matrix_2) == "jobsat2"] <- "Job satisfaction T2"
+# Categorical Working Hour Comparison Between the Years
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+data <- data[c(2:ncol(data))]
+# Get rid of the first letter X for each of the years
+colnames(data) <- gsub("X", "", colnames(data))
 
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDfwagency"] <- "FW T1 Agency and free will subscales"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDall"] <- "FW T1 All subscales combined"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDfw"] <- "FW T1 Free will subscale"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDagency"] <- "FW T1 Personal agency subscale"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDmoral"] <- "FW T1 Moral responsibility subscale"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDpower"] <- "FW T1 Higher power control subscale (R)"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDresp"] <- "FW T1 Personal responsibility subscale"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWDlimit"] <- "FW T1 Personal limitations subscale (R)"
+Data_1998 <- select(data,work_hours,`1998`)
+ggplot(Data_1998,aes(x=work_hours,y=`1998`)) +
+  geom_bar(stat="identity") + 
+  theme_minimal() + # Make the theme neater
+  labs(x = "Working Hours", y = "Number of Reports") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
 
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWself"] <- "FW T1 - Personal"
-rownames(correlation_matrix_2)[rownames(correlation_matrix_2) == "FWgeneral"] <- "FW T1 - General"
+Data_2008 <- select(data,work_hours,`2008`)
+ggplot(Data_2008,aes(x=work_hours,y=`2008`)) +
+  geom_bar(stat="identity") + 
+  theme_minimal() + # Make the theme neater
+  labs(x = "Working Hours", y = "Number of Reports") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
 
-#### Table 3-Controlling for demographics#####################################################################################
-selected_variables_3 <-data[, c("FWgeneral","jobsat1", "jobsat2","age","gender")]
+Data_2021 <- select(data,work_hours,`2021`)
+ggplot(Data_2021,aes(x=work_hours,y=`2021`)) +
+  geom_bar(stat="identity") + 
+  theme_minimal() + # Make the theme neater
+  labs(x = "Working Hours", y = "Number of Reports") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
 
-# Compute pairwise correlations
-correlation_matrix_3 <- cor(selected_variables_3, use = "pairwise.complete.obs")
-#choose desired rows and columns
-correlation_matrix_3 <- correlation_matrix_3[,c('FWgeneral','jobsat1','jobsat2')]
+Data_Total <- select(data,work_hours,Total)
+ggplot(Data_Total,aes(x=work_hours,y= Total)) +
+  geom_bar(stat="identity") + 
+  theme_minimal() + # Make the theme neater
+  labs(x = "Working Hours", y = "Number of Reports") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
 
-#rename columns and rows 
-colnames(correlation_matrix_3)[colnames(correlation_matrix_3) == "FWgeneral"] <- "Belief in free will (T1)"
-colnames(correlation_matrix_3)[colnames(correlation_matrix_3) == "jobsat1"] <- "Job satisfaction (T1)"
-colnames(correlation_matrix_3)[colnames(correlation_matrix_3) == "jobsat2"] <- "Job satisfaction (T2)"
+# Nonresponse Rates
+# Calculate percentages
+data <- read.csv(here:here("/input/data/cleaned_categorized_data.csv"))
+data <- data[c(2:ncol(data))]
+data <- data[1:6,]
 
-rownames(correlation_matrix_3)[rownames(correlation_matrix_3) == "FWgeneral"] <- "Belief in free will (T1)"
-rownames(correlation_matrix_3)[rownames(correlation_matrix_3) == "jobsat1"] <- "Job satisfaction (T1)"
-rownames(correlation_matrix_3)[rownames(correlation_matrix_3) == "jobsat2"] <- "Job satisfaction (T2)"
-rownames(correlation_matrix_3)[rownames(correlation_matrix_3) == "age"] <- "Age"
-rownames(correlation_matrix_3)[rownames(correlation_matrix_3) == "gender"] <- "Gender"
+#colnames(data) <- paste(colnames(data),"Year",sep="_") 
+
+colnames(data)<- as.character(colnames(data))
+
+#data_long <- data %>% 
+#  pivot_longer(cols = everything(), names_to = "WorkHours", values_to #= "Response")
+
+# Select the 'No Response' row and remove the first column which is not needed for the plot
+no_response_data <- data[data$work_hours == 'No Response', -1]
+
+# We also need to remove the 'Total' column as we did in the Python example
+no_response_data <- no_response_data[-length(no_response_data)]
+
+# Calculate total responses per year excluding the 'work_hours' and 'Total' columns
+
+data <- read.csv("cleaned_categorized_data.csv")
+data <- data[c(3:ncol(data))]
+
+total_responses_per_year <- as.vector(data[7,])
+total_responses_per_year <- unlist(total_responses_per_year)
+
+# Calculate non-response rate as a percentage
+non_response_rate <- (no_response_data / total_responses_per_year) * 100
+
+years <- colnames(non_response_rate)
+years <- gsub("X", "", years)
+
+non_response_rate <-as.vector(non_response_rate)
+non_response_rate <-unlist(non_response_rate)
+non_response_data <- data.frame(years,non_response_rate)
+
+# Plot using ggplot2
+ggplot(non_response_data, aes(x = years,y=non_response_rate)) +
+  geom_bar(stat = "identity",fill = "skyblue") +
+  theme_minimal() +
+  labs(title = "Non-Response Rate by Year", x = "Year", y = "Percentage of Non-Responses") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
