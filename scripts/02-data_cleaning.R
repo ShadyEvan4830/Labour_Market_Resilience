01c3281c7bb1d344103c1d9882646be2919b3757
Mingjia Chen <mingjia.chen@mail.utoronto.ca>
2024-03-18 00:32:16 -0400

    Update new dataset

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -13,6 +13,7 @@ library(knitr)
 library(dplyr)
 library(kableExtra)
 library(here)
+library(haven)
 
 #### Clean data ####
 # load dataset that has been cropped for analysis
@@ -100,3 +101,49 @@ colnames(cate_data) <- year
 
 
 write.csv(cate_data,"data/analysis_data/cleaned_categorized_data.csv")
+
+##### Additional Data Set for Result Section ######
+#Code Referenced from GSS website
+#https://gssdataexplorer.norc.org/MyGSS
+library(foreign)
+read.dct <- function(dct, labels.included = "yes") {
+  temp <- readLines(dct)
+  temp <- temp[grepl("_column", temp)]
+  switch(labels.included,
+         yes = {
+           pattern <- "_column\\(([0-9]+)\\)\\s+([a-z0-9]+)\\s+(.*)\\s+%([0-9]+)[a-z]\\s+(.*)"
+           classes <- c("numeric", "character", "character", "numeric", "character")
+           N <- 5
+           NAMES <- c("StartPos", "Str", "ColName", "ColWidth", "ColLabel")
+         },
+         no = {
+           pattern <- "_column\\(([0-9]+)\\)\\s+([a-z0-9]+)\\s+(.*)\\s+%([0-9]+).*"
+           classes <- c("numeric", "character", "character", "numeric")
+           N <- 4
+           NAMES <- c("StartPos", "Str", "ColName", "ColWidth")
+         })
+  temp_metadata <- setNames(lapply(1:N, function(x) {
+    out <- gsub(pattern, paste("\\", x, sep = ""), temp)
+    out <- gsub("^\\s+|\\s+$", "", out)
+    out <- gsub('\"', "", out, fixed = TRUE)
+    class(out) <- classes[x] ; out }), NAMES)
+  temp_metadata[["ColName"]] <- make.names(gsub("\\s", "", temp_metadata[["ColName"]]))
+  temp_metadata
+}
+
+read.dat <- function(dat, metadata_var, labels.included = "yes") {
+  read.table(dat, col.names = metadata_var[["ColName"]])
+}
+
+
+GSS_metadata <- read.dct(here::here("data/raw_data/GSS.dct"))
+GSS_ascii <- read.dat(here::here("data/raw_data/GSS.dat"), GSS_metadata)
+attr(GSS_ascii, "col.label") <- GSS_metadata[["ColLabel"]]
+additional_data <- GSS_ascii
+
+additional_data <- select(additional_data, AGE, SEX, YEAR)
+
+write.csv(additional_data,"data/analysis_data/supplementary_data.csv")
+
+
+
